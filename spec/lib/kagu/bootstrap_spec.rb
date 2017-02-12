d3c115806c5b8646411bc73cc41a23809ec31d63
David Stancu <mach@Octal.hubble.lan>
2017-02-12 16:11:57 -0500

    [#139642741] fix test

--- spec/lib/kagu/bootstrap_spec.rb
+++ spec/lib/kagu/bootstrap_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Kagu::Bootstrap do
     it 'should class alias all top level models' do 
       expect(Object.constants).to include(*Kagu::Models.constants)
     end
+
+    it 'should have class types' do 
+      expect { Kagu::Models.constants.map {|c| c.to_s.constantize} }
+        .to_not raise_error
+    end
   end
 end

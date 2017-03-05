d789a7a8f1cf843afa9d7ece8ea857fd3b7d6457
Paulina Levit <levit@nyu.edu>
2017-03-05 17:15:36 -0500

    foreign keys and spacing issues fixed
    [#140191879]

--- db/schema.rb
+++ db/schema.rb
@@ -69,4 +69,11 @@ ActiveRecord::Schema.define(version: 20170305160915) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "experiments", "users"
+  add_foreign_key "samples", "users"
+  add_foreign_key "samples_experiments", "experiments"
+  add_foreign_key "samples_experiments", "samples"
+  add_foreign_key "scores", "experiments"
+  add_foreign_key "scores", "samples"
+  add_foreign_key "scores", "users"
 end

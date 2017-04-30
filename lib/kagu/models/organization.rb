2208ee14034579e987adf4b46dbcd15d6c7e85f0
David Stancu <dstancu@nyu.edu>
2017-04-29 23:17:28 -0400

    fix indents
    [#142475501]

--- lib/kagu/models/organization.rb
+++ lib/kagu/models/organization.rb
@@ -5,21 +5,21 @@ module Kagu
       include Tags::Taggable
 
       has_many :organization_mappings
-      
-      has_many :users, 
-        through: :organization_mappings, 
-        source: :kindable,
-        source_type: User.name
 
-      has_many :experiments, 
-        through: :organization_mappings, 
-        source: :kindable,
-        source_type: Experiment.name
+      has_many :users,
+               through: :organization_mappings,
+               source: :kindable,
+               source_type: User.name
 
-      has_many :samples, 
-        through: :organization_mappings, 
-        source: :kindable,
-        source_type: Sample.name
+      has_many :experiments,
+               through: :organization_mappings,
+               source: :kindable,
+               source_type: Experiment.name
+
+      has_many :samples,
+               through: :organization_mappings,
+               source: :kindable,
+               source_type: Sample.name
 
       alias_attribute :resources, :kindable
     end
